f6e9e03e975fa9ff7603e1c02ec6b85797706751
Karan <karansbisht7@gmail.com>
2024-08-09 17:22:44 +0530

    Created boilerplate for ejs pages and navbar

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const db = require("./config/db");
 db();
@@ -13,6 +14,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 const listingRoute = require("./routes/listingRoute");
 const userRoute = require("./routes/userRoute");
